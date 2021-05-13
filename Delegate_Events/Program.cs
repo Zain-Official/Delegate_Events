84dfda1aaa49aef9fbd8cd90a0555d869fa13de4
Zain <zainofficial03@gmail.com>
2021-05-14 01:23:48 +0500

    Add project files.

---
+++ Delegate_Events/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public delegate void onEventHandler(object sender, EventArgs e);
+namespace Delegate_Events
+{
+    class Program
+    {
+        public static void Callback(object sender,EventArgs e)
+        {
+            Console.WriteLine("Got Five Numbers");
+        }
+        static void Main(string[] args)
+        {
+            fEvent fevt = new fEvent();
+            fevt.FiveEvent += new onEventHandler(Callback);
+            Random random = new Random();
+            for(int i=0; i<5; i++)
+            {
+                int ro = random.Next(6);
+                Console.WriteLine(ro);
+                if(ro == 5)
+                {
+                    fevt.GotFive();
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Delegate_Events/fEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Delegate_Events
+{
+    class fEvent
+    {
+        public event onEventHandler FiveEvent;
+        public void GotFive()
+        {
+            FiveEvent(this,EventArgs.Empty);
+        }
+    }
+}
